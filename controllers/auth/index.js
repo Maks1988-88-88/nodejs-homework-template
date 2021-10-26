b9ef3f2a14c89d16e69dea990cde2de174914c8c
Maksim Tsopa <maks98242@gmail.com>
2021-10-26 22:51:38 +0300

    final

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,14 +3,20 @@ const signin = require("./signin");
 const signout = require("./signout");
 const getCurrentUser = require("./getCurrentUser");
 const addUserAvatar = require("./addUserAvatar");
+const verify = require("./verify");
+const reverify = require("./reverify");
+
+
 
 
 
 
 module.exports = {
   signup,
+  verify,
   signin,
   signout,
   getCurrentUser,
   addUserAvatar,
+  reverify,
 };
---
+++ controllers/auth/verify.js
@@ -0,0 +1,17 @@
+const { NotFound } = require("http-errors");
+const { User } = require("../../models/");
+const { sendSuccessRes } = require("../../helpers");
+
+const verify = async (req, res) => {
+  const { verifyToken } = req.params;
+  const user = await User.findOne({ verifyToken });
+
+  if (!user) {
+    throw new NotFound("User do not exist or not verify");
+  }
+
+  await User.findByIdAndUpdate(user._id, { verify: true, verifyToken: null });
+  sendSuccessRes(res, { message: "Verification successful" }, 200);
+};
+
+module.exports = verify;
